340b9c2bd0811e4389ac8064ec42e0562a0391ee
Tien Le <50081744+tiennynyle@users.noreply.github.com>
2020-05-13 07:45:54 -0400

    Styled code adhering to the PEP 8 style guide

--- models.py
+++ models.py
@@ -1,11 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import (Flask, render_template,
-                    request, Response, redirect, url_for, jsonify, abort)
+from flask import (Flask, render_template, request,
+                   Response, redirect, url_for, jsonify, abort)
 
 
 database_name = "capstone"
 database_path = "postgres://admeokiwakdldw:29015e30c1e200707aeca46f6a5fd212e75d7c112e1aa692568150533dbeca67@ec2-3-223-21-106.compute-1.amazonaws.com:5432/dc2h6aqcn1mdje"
 db = SQLAlchemy()
+
 
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -18,14 +19,14 @@ def setup_db(app, database_path=database_path):
 class Movie(db.Model):
     __tablename__ = 'Movie'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
     release_date = db.Column(db.Date)
 
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-    
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
@@ -33,10 +34,11 @@ class Movie(db.Model):
     def update(self):
         db.session.commit()
 
-class Actor(db.Model):
-    __tablename__= 'Actor'
 
-    id = db.Column(db.Integer, primary_key = True)
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     age = db.Column(db.Integer)
     gender = db.Column(db.String)
